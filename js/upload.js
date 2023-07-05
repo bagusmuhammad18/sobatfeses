32a8c7885c6f4d6765a5a7e8760ef25bb63d8bbf
bagusmuhammad18 <bagusmuhammadfajar27@gmail.com>
2023-07-06 02:39:48 +0700

    bug fixed on popup mobile

--- js/upload.js
+++ js/upload.js
@@ -19,16 +19,19 @@ window.addEventListener("resize", function () {
   }
 });
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
+
+// Get saya mengerti button
+let mengerti = document.getElementsByClassName("mengerti")[0];
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("webcam-custom")[0];
+let btn = document.getElementsByClassName("webcam-custom")[0];
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // Get the input element
-var webcamUploadInput = document.getElementById("webcamUpload");
+let webcamUploadInput = document.getElementById("webcamUpload");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
@@ -37,6 +40,11 @@ btn.onclick = function () {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks on Saya Mengerti, close the modal
+mengerti.onclick = function () {
   modal.style.display = "none";
 };
 
